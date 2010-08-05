be059a46171a756cbd837cf9008d210eb57ac427
Frank Cieslik <hello@fcn.nu>
2010-08-05 21:26:38 +0200

    adding .htaccess information changes

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -1,6 +1,35 @@
 class ContactController < ApplicationController
-  
+  require 'vpim/vcard'
   def index
+
+  end
+  
+  def vcard
+    card = Vpim::Vcard::Maker.make2 do |maker|
+      maker.add_name do |name|
+        name.prefix = ''
+        name.given = USER_INFO[:firstname]
+        name.family = USER_INFO[:lastname]
+      end
+    
+      maker.add_addr do |addr|
+        addr.preferred = true
+        addr.location = USER_INFO[:location]
+        addr.street = USER_INFO[:street]
+        addr.locality = USER_INFO[:locality]
+        addr.postalcode = USER_INFO[:zip]
+        addr.region = USER_INFO[:region]
+        addr.country = USER_INFO[:country]
+      end
+
+
+
+      #maker.add_tel(@user.telephone)
+      #maker.add_email(@user.email) { |e| e.location = 'work' }
+
+    end
+
+    send_data card.to_s, :filename => "contact.vcf"
     
   end
   
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,31 +1,41 @@
 var twitter_scroll = {
-  act : 0,
+  act : 1,
   max : 10,
   
   next: function(){
-    if(twitter_scroll.act >= twitter_scroll.max-1) {
-      $('#twitter_scroll_next').addClass('grey');
-    }
-    else if (twitter_scroll.act < twitter_scroll.max){
+    
+    if (twitter_scroll.act < twitter_scroll.max){
       $('#twitter_scroll_'+twitter_scroll.act).hide();
       twitter_scroll.act++;
       $('#twitter_scroll_'+twitter_scroll.act).show();
       $('#twitter_scroll_prev').removeClass('grey');
     }
+    
+    if(twitter_scroll.act == twitter_scroll.max) {
+      $('#twitter_scroll_next').addClass('grey');
+    }
+    
   },
   prev : function() {
-    if(twitter_scroll.act <= 1) {
-      $('#twitter_scroll_prev').addClass('grey');
-    }
-    else if (twitter_scroll.act > 0) {
+    
+    if (twitter_scroll.act > 1) {
       $('#twitter_scroll_'+twitter_scroll.act).hide();
       twitter_scroll.act--;
       $('#twitter_scroll_'+twitter_scroll.act).show();
       $('#twitter_scroll_next').removeClass('grey');
     }
+    
+    if(twitter_scroll.act == 1) {
+      $('#twitter_scroll_prev').addClass('grey');
+    }
+    
+  },
+  set_max : function(max) {
+    if(max < twitter_scroll.max)
+      twitter_scroll.max = max;
   }
   
 }
 $(document).ready(function(){
-   
+   twitter_scroll.max = $('#tweets li').length
 });
